977540209829e93a126e534b8d5f7ee172ad0973
luisgustavo-mm <luisgustavomm.dev@gmail.com>
2025-07-04 22:40:02 -0300

    creation and removal of key files for the project

---
+++ src/TodoList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import './TodoList.css'
+
+function TodoList() {
+  return (
+    <>
+      <div>
+        <h1>Lista de Tarefas</h1>
+        <form>
+          <input type="text" placeholder="Adicione uma tarefa" />
+          <button className="add" type="submit">
+            Add
+          </button>
+        </form>
+      </div>
+      <div className="listaTarefas">
+        <div className="item">
+          <span>Tarefa de exemplo</span>
+          <button className="del">Deletar</button>
+        </div>
+        <div className="item completo">
+          <span>Tarefa de exemplo</span>
+          <button className="del">Deletar</button>
+        </div>
+        <button className="deleteAll">Deletar Todas</button>
+      </div>
+    </>
+  )
+}
+
+export default TodoList
